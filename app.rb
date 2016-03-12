bdcd12a347312ffe40a26977227ff7ed7e93ea76
Jenn Eng <Jenn@Jennifers-MacBook-Pro-2.local>
2016-03-12 16:55:34 -0500

    Initial commit

---
+++ app.rb
@@ -0,0 +1,22 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require "sinatra/activerecord"
+require 'pry'
+
+require_relative 'models/contact'
+also_reload 'models/contact'
+
+
+
+
+
+get '/' do
+  @contacts = Contact.all
+  erb :index
+end
+
+get '/contacts/:id' do
+  @contact = Contact.find(params[:id])
+  # binding.pry
+  erb :show
+end
